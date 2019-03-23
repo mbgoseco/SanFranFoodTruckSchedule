68e8746a662645ceeadd852032eb41b5badae5a8
Michael Goseco <mbgoseco@gmail.com>
2019-03-23 01:05:51 -0700

    Project file structure setup.

---
+++ SF_FoodTrucks/SF_FoodTrucks/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace SF_FoodTrucks
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
